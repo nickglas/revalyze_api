96235a90902033a19c3393282ed00c813a29433a
Nick Glas <nickglas@hotmail.nl>
2025-06-14 13:51:10 +0200

    Fixed jest

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,8 @@ module.exports = {
     preset: 'ts-jest',
     silent: false,
     testEnvironment: 'node',
-    testMatch: ['**/test/**/*.test.ts'],
+    testMatch: ['**/src/test/**/*.test.ts'],
     moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-    roots: ['<rootDir>/src', '<rootDir>/test'],
-    setupFilesAfterEnv: ['<rootDir>/test/setup.ts'],
-  };
-  
+    roots: ['<rootDir>/src'],
+    setupFilesAfterEnv: ['<rootDir>/src/test/setup.ts'],
+};
