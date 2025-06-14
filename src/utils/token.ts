e27b8a8d4e03a6a9782ed58d784560945b5ba12f
Nick Glas <nickglas@hotmail.nl>
2025-06-14 13:44:37 +0200

    Added additional logging

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -2,15 +2,20 @@ import jwt from 'jsonwebtoken';
 import { IUser } from '../models/user.model';
 
 export const generateTokens = (user: IUser) => {
+
+  const jwtSecret = process.env.JWT_SECRET;
+  if (!jwtSecret) throw new Error('JWT_SECRET not defined');
   const accessToken = jwt.sign(
     { id: user._id, role: user.role, email: user.email },
-    process.env.JWT_SECRET!,
+    jwtSecret!,
     { expiresIn: '15m' }
   );
 
+  const jwtRefreshSecret = process.env.JWT_REFRESH_SECRET;
+  if (!jwtRefreshSecret) throw new Error('JWT_REFRESH_SECRET not defined');
   const refreshToken = jwt.sign(
     { id: user._id },
-    process.env.JWT_REFRESH_SECRET!,
+    jwtRefreshSecret!,
     { expiresIn: '7d' }
   );
 
