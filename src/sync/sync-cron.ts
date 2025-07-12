a9c85304812f5633b8943fb335e0e488ebf6a073
Nick Glas <nickglas@hotmail.nl>
2025-07-12 18:49:02 +0200

    Sync - Added dynamic cron time based on env

--- src/sync/sync-cron.ts
+++ src/sync/sync-cron.ts
@@ -3,6 +3,8 @@ import { Service } from "typedi";
 import { StripeSyncService } from "../services/stripe.sync.service";
 import { logger } from "../utils/logger";
 
+const CRON_WAIT_TIME = 30;
+
 @Service()
 export class StripeSyncCron {
   private job?: ScheduledTask;
@@ -16,26 +18,36 @@ export class StripeSyncCron {
       return;
     }
 
-    logger.info("Scheduling cron job for every 30 minutes...");
-    this.job = cron.schedule("*/30 * * * *", async () => {
-      if (this.isRunning) {
-        logger.info("Previous sync still running, skipping this run");
-        return;
-      }
+    logger.info(
+      `Scheduling cron job for every ${
+        process.env.NODE_ENV === "development" ? "1" : CRON_WAIT_TIME
+      } minutes...`
+    );
 
-      this.isRunning = true;
-      logger.info("Starting Stripe sync process...");
-      try {
-        await this.stripeSyncService.syncProducts();
-        await this.stripeSyncService.syncPendingSubscriptions();
-        await this.stripeSyncService.syncCompanies();
-        await this.stripeSyncService.syncSubscriptions();
-        logger.info("Stripe sync process complete");
-      } catch (error) {
-        logger.error("Stripe sync process failed:", error);
-      } finally {
-        this.isRunning = false;
+    this.job = cron.schedule(
+      process.env.NODE_ENV === "development"
+        ? "* * * * *"
+        : `*/${CRON_WAIT_TIME} * * * *`,
+      async () => {
+        if (this.isRunning) {
+          logger.info("Previous sync still running, skipping this run");
+          return;
+        }
+
+        this.isRunning = true;
+        logger.info("Starting Stripe sync process...");
+        try {
+          await this.stripeSyncService.syncProducts();
+          await this.stripeSyncService.syncPendingSubscriptions();
+          await this.stripeSyncService.syncCompanies();
+          await this.stripeSyncService.syncSubscriptions();
+          logger.info("Stripe sync process complete");
+        } catch (error) {
+          logger.error("Stripe sync process failed:", error);
+        } finally {
+          this.isRunning = false;
+        }
       }
-    });
+    );
   }
 }
